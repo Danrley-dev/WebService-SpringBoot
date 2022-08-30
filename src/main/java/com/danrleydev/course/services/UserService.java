2a14a5658e677ef083386bc068f097e1e746c724
Danrleydev <danrleysouza53@gmail.com>
2022-08-30 13:39:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/danrleydev/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.danrleydev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danrleydev.course.entities.User;
+import com.danrleydev.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
