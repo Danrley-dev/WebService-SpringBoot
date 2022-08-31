4287204c574e13a11839da3cb0a98823b3e4c43f
Danrleydev <danrleysouza53@gmail.com>
2022-08-31 14:48:19 -0300

    Payment, one-to-one association

--- src/main/java/com/danrleydev/course/config/TestConfig.java
+++ src/main/java/com/danrleydev/course/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.danrleydev.course.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.danrleydev.course.entities.Category;
 import com.danrleydev.course.entities.Order;
 import com.danrleydev.course.entities.OrderItem;
+import com.danrleydev.course.entities.Payment;
 import com.danrleydev.course.entities.Product;
 import com.danrleydev.course.entities.User;
 import com.danrleydev.course.entities.enums.OrderStatus;
@@ -81,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1= new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 }
